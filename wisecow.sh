75edd09e1acbcf8fee0315b6f4991d4f768a59ce
Cherukuri Ashik <135224271+Ashik-Domain@users.noreply.github.com>
2025-10-08 12:18:03 +0530

    Updated wisecow.sh

--- wisecow.sh
+++ wisecow.sh
@@ -1,46 +1,30 @@
 #!/usr/bin/env bash
 
+export PATH=$PATH:/usr/games
+
 SRVPORT=4499
-RSPFILE=response
-
-rm -f $RSPFILE
-mkfifo $RSPFILE
-
-get_api() {
-	read line
-	echo $line
-}
 
 handleRequest() {
-    # 1) Process the request
-	get_api
-	mod=`fortune`
-
-cat <<EOF > $RSPFILE
+    mod=$(fortune)
+    cat <<EOF
 HTTP/1.1 200
 
-
-<pre>`cowsay $mod`</pre>
+<pre>$(cowsay "$mod")</pre>
 EOF
 }
 
 prerequisites() {
-	command -v cowsay >/dev/null 2>&1 &&
-	command -v fortune >/dev/null 2>&1 || 
-		{ 
-			echo "Install prerequisites."
-			exit 1
-		}
+    command -v cowsay >/dev/null 2>&1 || { echo "cowsay not found"; exit 1; }
+    command -v fortune >/dev/null 2>&1 || { echo "fortune not found"; exit 1; }
 }
 
 main() {
-	prerequisites
-	echo "Wisdom served on port=$SRVPORT..."
+    prerequisites
+    echo "Wisdom server starting on port $SRVPORT..."
 
-	while [ 1 ]; do
-		cat $RSPFILE | nc -lN $SRVPORT | handleRequest
-		sleep 0.01
-	done
+    while true; do
+        nc -l -p $SRVPORT < <(handleRequest)
+    done
 }
 
 main
